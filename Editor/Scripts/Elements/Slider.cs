835600d8cabafdafcb94bb586dd1bb5d6097fd8b
Sebastian Inman <hello@sebastianinman.me>
2022-03-23 14:34:56 -0700

    Added slider field

---
+++ Editor/Scripts/Elements/Slider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UIElements;
+
+namespace ExtendedEditorGUI.Elements {
+
+    public struct SliderAttributes<T> {
+        public string name;
+        public float defaultValue;
+        public float lowValue;
+        public float highValue;
+    }
+    
+    public class Slider<T> : Element<UnityEngine.UIElements.Slider> {
+
+        public float value;
+        
+        public Slider(SliderAttributes<T> attributes, VisualElement template) : base(attributes.name, template) {
+            element = template.Q<UnityEngine.UIElements.Slider>(name);
+            element.lowValue = attributes.lowValue;
+            element.highValue = attributes.highValue;
+            element.value = attributes.defaultValue;
+        }
+
+        public void OnChange(EventCallback<Slider<T>> changeEvent) {
+            element?.RegisterCallback<ChangeEvent<float>>(@event => {
+                value = @event.newValue;
+                changeEvent(this);
+            });
+        }
+
+    }
+    
+}
--- Editor/Scripts/GUI.cs
+++ Editor/Scripts/GUI.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.UIElements;
+﻿using ExtendedEditorGUI.Elements;
+using UnityEngine;
 
 namespace ExtendedEditorGUI {
 
@@ -24,36 +24,38 @@ namespace ExtendedEditorGUI {
         public static readonly string WindowStylesheet = $"{BaseStylesPath}/window.uss";
         public static readonly string InspectorStylesheet = $"{BaseStylesPath}/inspector.uss";
 
-        public readonly VisualElement root;
+        public readonly UnityEngine.UIElements.VisualElement root;
 
-        public GUI(VisualElement rootVisualElement) {
-            
+        public GUI(UnityEngine.UIElements.VisualElement rootVisualElement) {
             root = rootVisualElement;
-            
         }
 
-        public Elements.Element<VisualElement> Element(string elementName) {
-            return new Elements.Element<VisualElement>(elementName, root);
+        public Element<UnityEngine.UIElements.VisualElement> Element(string elementName) {
+            return new Element<UnityEngine.UIElements.VisualElement>(elementName, root);
         }
         
-        public Elements.Button Button(string buttonName) {
-            return new Elements.Button(buttonName, root);
+        public Button Button(string buttonName) {
+            return new Button(buttonName, root);
         }
         
-        public Elements.Field<TFieldType> Field<TFieldType>(string fieldName) {
-            return new Elements.Field<TFieldType>(fieldName, root);
+        public Field<TFieldType> Field<TFieldType>(string fieldName) {
+            return new Field<TFieldType>(fieldName, root);
         }
         
-        public Elements.Field<TFieldType> Field<TFieldType>(string fieldName, TFieldType defaultValue) {
-            return new Elements.Field<TFieldType>(fieldName, defaultValue, root);
+        public Field<TFieldType> Field<TFieldType>(string fieldName, TFieldType defaultValue) {
+            return new Field<TFieldType>(fieldName, defaultValue, root);
+        }
+
+        public Slider<TField> Slider<TField>(SliderAttributes<TField> attributes) {
+            return new Slider<TField>(attributes, root);
+        }
+
+        public ObjectField<TObjectType> ObjectField<TObjectType>(string fieldName) where TObjectType : Object {
+            return new ObjectField<TObjectType>(fieldName, root);
         }
         
-        public Elements.ObjectField<TObjectType> ObjectField<TObjectType>(string fieldName) where TObjectType : Object {
-            return new Elements.ObjectField<TObjectType>(fieldName, root);
-        }
-        
-        public Elements.ObjectField<TObjectType> ObjectField<TObjectType>(string fieldName, TObjectType defaultValue) where TObjectType : Object {
-            return new Elements.ObjectField<TObjectType>(fieldName, defaultValue, root);
+        public ObjectField<TObjectType> ObjectField<TObjectType>(string fieldName, TObjectType defaultValue) where TObjectType : Object {
+            return new ObjectField<TObjectType>(fieldName, defaultValue, root);
         }
         
     }
